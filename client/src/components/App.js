6a46594dc194292f5dc9138dd2f6b89915f415d8
Andrew <atg27@uw.edu>
2021-07-23 19:32:18 -0700

    added hooks to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,21 @@
 
 import '../styles/App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
 import Home from './Home'
 import Navbar from './NavBar'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [user, setUser] = useState({})
+  const history = useHistory()
+  
   return (
     <div className="App">
       <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
-      <h3>my react app</h3>
     </div>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,10 +5,19 @@ import { NavLink, Link } from 'react-router-dom'
 const Navbar = (props) => {
 
     if (props.loggedIn) {
-
+        return (
+            <div>
+                <h1>Hello {props.user.name}!</h1>
+                <br/>
+                <Link to="/signup" >
+                    <button>Signup</button>
+                </Link>
+            </div>
+        )
     } else {
         return (
         <div>
+            <br/>
             <Link to="/signup" >
                 <button>Signup</button>
             </Link>
