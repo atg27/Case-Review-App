e4e37db6137c4f877d06e17667ae2100e8914bc4
Andrew <atg27@uw.edu>
2021-07-23 19:56:09 -0700

    set up sessions controller, create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
     def create #login
-
+        user = User.find_by(name: params[:name])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id 
+            render json: user, status: :created
+        else 
+            render json: {error: "Invalid Username or Password"}, status: :unauthorized
+        end
     end
 
     def destory #logout
-        
+        session.delete :user_id
+        head :no_content
     end
 end
